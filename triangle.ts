16230503017a3b884208f2f0aed98edb64924942
Shawn Cannon <scanno16@ford.com>
2022-05-10 12:59:53 -0600

    pass all tests

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,57 @@
-export class Triangle {}
+export class Triangle {
+    side1: number;
+    side2: number;
+    side3: number;
+
+    constructor(side1: number, side2: number, side3: number) {
+
+        this.side1 = side1
+        this.side2 = side2
+        this.side3 = side3
+    }
+
+    private get confirmTriangle() {
+        return (this.conformToSideLaw &&
+            this.side1 > 0 && this.side2 > 0 && this.side3 > 0)
+    }
+
+    get conformToSideLaw() {
+        return (this.side1 + this.side2 >= this.side3
+            && this.side3 + this.side2 >= this.side1
+            && this.side1 + this.side3 >= this.side2)
+    }
+
+    get isEquilateral() {
+        return this.confirmTriangle && this.side1 === this.side2 && this.side1 === this.side3
+    }
+
+    get isIsosceles() {
+
+        return (
+            this.confirmTriangle &&
+            (this.side1 === this.side2 ||
+                this.side2 === this.side3 ||
+                this.side3 === this.side1)
+        )
+        /*
+        if (this.side1 === this.side2 && this.side1 > this.side3) {
+            return this.confirmTriangle
+        } else if (this.side2 === this.side3 && this.side2 > this.side1) {
+            return this.confirmTriangle
+        } else if (this.side1 === this.side3 && this.side1 > this.side2) {
+            return this.confirmTriangle
+        } else if (this.side1 === this.side2 && this.side1 === this.side3) {
+            return this.confirmTriangle
+        }
+        */
+    }
+
+    get isScalene() {
+        return (this.confirmTriangle &&
+            !this.isEquilateral &&
+            !this.isIsosceles)
+    }
+
+
+}
+
